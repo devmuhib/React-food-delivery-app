084e4fcbb6fdf412829ff30d1f2f37f0873372e3
Hanna <hannageissler@googlemail.com>
2023-02-13 11:25:22 +0100

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,41 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
+import logo from "../../assets/images/res-logo.png";
 
-import React from 'react'
+import "../../styles/footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>MyPizza</h5>
+              <p>Best Pizzas in town, try it out!</p>
+            </div>
+          </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delifery Time</h5>
+            <ListGroup>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday - Tuesday</span>
+                <p>10:00am - 11:00 pm</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0  ">
+                <span>Wednesday - Thursday</span>
+                <p>Off day </p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
