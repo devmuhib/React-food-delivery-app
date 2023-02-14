30c1e25bb2870fb582e321595d55eacecb98973e
Hanna <hannageissler@googlemail.com>
2023-02-14 10:57:27 +0100

    add pizza-page without styling

---
+++ src/components/UI/product-card/ProductCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// import productImg from "../../../assets/images/product_2.1.jpg";
+
+import { Link } from "react-router-dom";
+
+const ProductCard = (props) => {
+  const { id, title, image01, price } = props.item;
+
+  return (
+    <div className="product__item">
+      <div className="product__img">
+        <img src={image01} alt="Pizza" className="w-100" />
+      </div>
+      <div className="product__content">
+        <h5>
+          <Link to={`/foods(${id})`}>{title}</Link>
+        </h5>
+        <div>
+          <span className="product__price">{price} € </span>
+          <button className="addTOCART__btn">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Pizzas.jsx
+++ src/pages/Pizzas.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Container, Row, Col } from "reactstrap";
+
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCard";
+import Helmet from "../components/Helmet/Helmet";
+import ReactPaginate from "react-paginate";
 
 const Pizzas = () => {
-  return (
-    <div>Pizzas</div>
-  )
-}
+  const [pageNumber, setPageNumber] = useState(0);
 
-export default Pizzas
+  const searchedProduct = products;
+
+  const productPerPage = 12;
+  const visitedPage = pageNumber * productPerPage;
+  const displayPage = searchedProduct.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+
+  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <Helmet title="All Pizzas">
+      <Container>
+        <Row>
+          {displayPage.map((item) => (
+            <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+              <ProductCard item={item} />
+            </Col>
+          ))}
+          <div>
+            <ReactPaginate
+              pageCount={pageCount}
+              onPageChange={changePage}
+              previousLabel={"Prev"}
+              nextLabel={"Next"}
+              containerClassName="paginationBttns"
+            />
+          </div>
+        </Row>
+      </Container>
+    </Helmet>
+  );
+};
+
+export default Pizzas;
