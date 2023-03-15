7b1196e5af18a4631e71b4b10c8d6dcb42fe8826
canhoilho <freitasc941@gmail.com>
2023-03-15 14:56:45 +0100

    Center footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,32 +7,24 @@ import "../../styles/footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
-        <Row>
-          <Col lg="3" md="4" sm="6">
-            <div className="footer__logo text-center text-md-start">
-              <img src={logo} alt="logo" />
-              <h5>MyPizza</h5>
-              <p>Best Pizzas in town, try it out!</p>
-            </div>
-          </Col>
-          <Col lg="3" md="4" sm="6" className="text-center mx-auto">
-            <h5 className="footer__title">Delivery Time</h5>
-            <ListGroup>
-              <ListGroupItem className="delivery__time-item border-0 ps-0">
-                <span>Friday - Tuesday</span>
-                <p>10:00am - 11:00pm</p>
-              </ListGroupItem>
-              <ListGroupItem className="delivery__time-item border-0 ps-0">
-                <span>Wednesday - Thursday</span>
-                <p>Off day</p>
-              </ListGroupItem>
-            </ListGroup>
-          </Col>
-          <Col lg="3" md="4" sm="6"></Col>
-          <Col lg="3" md="4" sm="6"></Col>
-        </Row>
-      </Container>
+        <div className="footer__logo">
+          <img src={logo} alt="logo" />
+          <h5>MyPizza</h5>
+          <p>Best Pizzas in town, try it out!</p>
+        </div>
+      <div>
+        <h5 className="footer__title mb-3">Delivery Time</h5>
+        <ListGroup>
+          <ListGroupItem className="delivery__time-item border-0 ps-0">
+            <span>Friday - Tuesday</span>
+            <p>10:00am - 11:00pm</p>
+          </ListGroupItem>
+          <ListGroupItem className="delivery__time-item border-0 ps-0">
+            <span>Wednesday - Thursday</span>
+            <p>Off day</p>
+          </ListGroupItem>
+        </ListGroup>
+      </div>
     </footer>
   );
 };
