8de8186688a92a748740a7d7aff84dab6a7b5594
canhoilho <freitasc941@gmail.com>
2023-03-16 14:23:13 +0100

    Fix footer position and buttons overlapping

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,7 +11,7 @@ const Layout = () => {
   const showCart = useSelector((state) => state.cartUi.cartIsVisible);
 
   return (
-    <div>
+    <div className="d-flex flex-column vh-100 justify-content-between">
       <Header />
       {showCart && <Carts />}
       <div>
--- src/components/UI/cart/CartItem.jsx
+++ src/components/UI/cart/CartItem.jsx
@@ -32,7 +32,7 @@ const CartItem = ({ item }) => {
 
   return (
     <ListGroupItem className="border-0 cart__item">
-      <div className="cart__item-info d-flex gap-2">
+      <div className="cart__item-info d-flex gap-4">
         <img src={image01} alt="product-img" />
 
         <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
